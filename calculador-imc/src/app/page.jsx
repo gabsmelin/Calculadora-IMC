b7de0bf91d87ff45f6a7b439d079ab34fc032e2c
gabsmelin <rm550202@fiap.com.br>
2024-01-03 21:45:10 -0300

    feat - lógica para calculos desenvolvida

--- calculador-imc/src/app/page.jsx
+++ calculador-imc/src/app/page.jsx
@@ -1,26 +1,74 @@
+'use client'
+import { useState } from 'react';
+import './globals.css'
+
 export default function Home() {
+
+  const [valor, setValor] = useState({
+    nome: "",
+    peso: "",
+    altura: ""
+  });
+
+  const handleChange = (e) => {
+    setValor((prevValor) => ({
+      ...prevValor,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  
+
+  const calcularImc = () => {
+    if(valor.nome !== '' && valor.altura !== "" && valor.peso !== '') {
+      const calculo = valor.peso / (valor.altura * valor.altura);
+      return calculo.toFixed(1);
+    }else{
+      return "0";
+    }
+  }
+
+
+  function estadoPessoa() {
+    if (calcularImc() < 18.5) {
+      return "Abaixo do peso"
+    } else if(calcularImc() > 18.5 && calcularImc() <= 24.9) {
+      return "Peso normal"
+    } else if(calcularImc() > 24.9 && calcularImc() <= 29.9) {
+      return "Sobrepeso"
+    } else if(calcularImc() > 29.9) {
+      return "Acima do peso"
+    } else {
+      return "Preencha todos os campos!";
+    }
+  }
+  const estado = estadoPessoa();
+
   return (
-    <div>
+    <div className='container'>
+      <h1>Calculadora de IMC</h1>
       <div className="formulario">
         <form action="">
-          <div>
-            <label htmlFor="nome">Nome:</label>
-            <input type="text" id="nome" name="Nome" placeholder="João"/>
+          <div className='input'>
+            <label htmlFor="nome">Nome:</label> 
+            <input type="text" id="nome" name="nome" placeholder="João" value={valor.nome} onChange={handleChange}/>
           </div>
 
-          <div>
+          <div className='input'>
             <label htmlFor="peso">Peso:</label>
-            <input type="text" id="peso" name="Peso" placeholder="68.5"/>
+            <input type="number" id="peso" name="peso" placeholder="68.5" value={valor.peso} onChange={handleChange}/>
           </div>
 
-          <div>
+          <div className='input'>
             <label htmlFor="altura">Altura:</label>
-            <input type="text" id="altura" name="Altura"/>
+            <input type="number" id="altura" name="altura" placeholder='1.70' value={valor.altura} onChange={handleChange}/>
           </div>
+
+  
         </form>
       </div>
       <div className="resposta">
-        <p></p>
+        <p id='resultado'>{valor.nome} seu imc é igual a {calcularImc()}, você se encontrar no estado de {estado}</p>
       </div>
     </div>
   )
